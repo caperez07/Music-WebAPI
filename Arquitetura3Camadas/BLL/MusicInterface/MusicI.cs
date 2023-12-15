2666d76c395dee9b3224f83bfe736cf7adfce4b7
Carolina Perez <Carolina.Perez@t-systems.com>
2023-12-15 11:15:39 -0300

    Implementando a Business Logic Layer

---
+++ Arquitetura3Camadas/BLL/MusicInterface/MusicI.cs
@@ -0,0 +1,23 @@
+﻿using BLL.MusicViewModelInterface;
+using Models.Music;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.MusicViewModelInterface
+{
+    public interface MusicI<MusicViewModel>
+    {
+        Task Add(MusicViewModel Object);
+
+        Task Delete(MusicViewModel Object);
+
+        Task Update(MusicViewModel Object);
+
+        Task<MusicViewModel> GetEntityById(int Id);
+
+        Task<List<MusicViewModel>> List();
+    }
+}
