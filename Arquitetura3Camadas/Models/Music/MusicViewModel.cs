f6f8fb8c00f61104d0fabba0f8df535d3fbaf890
Carolina Perez <Carolina.Perez@t-systems.com>
2023-12-15 11:14:00 -0300

    Implementando a Model

---
+++ Arquitetura3Camadas/Models/Music/MusicViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Music
+{
+    [Table("Tabela_de_Músicas")]
+    public class MusicViewModel
+    {
+        [Key]
+        [Column("MusicID")]
+        [Display(Name = "Código da música")]
+        public int MusicId { get; set; }
+
+        [Required]
+        [Column("MusicNAME")]
+        [Display(Name = "Nome da música")]
+        [MaxLength(255)]
+        public string MusicName { get; set; }
+
+        [Required]
+        [Column("SingerNAME")]
+        [Display(Name = "Nome do cantor(a/e)")]
+        [MaxLength(70)]
+        public string SingerName { get; set; }
+
+        [Required]
+        [Column("MusicGENDER")]
+        [Display(Name = "Gênero da Música")]
+        [MaxLength(15)]
+        public string MusicGender { get; set; }
+
+
+        [Column("MusicPLATFORMS")]
+        [Display(Name = "Plataformas disponíveis")]
+        [MaxLength(20)]
+        public string? MusicPlatforms { get; set; }
+
+        public MusicViewModel()
+        {
+        }
+
+        public MusicViewModel(int musicId, string musicName, string singerName, string musicGender, string? musicPlatforms)
+        {
+            MusicId = musicId;
+            MusicName = musicName;
+            SingerName = singerName;
+            MusicGender = musicGender;
+            MusicPlatforms = musicPlatforms;
+        }
+    }
+}
